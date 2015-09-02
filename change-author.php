a45c41adb9ae11d61e8d8c16d72d819055124f56
Primož Bevk <primoz.bevk@outfit7.com>
2015-09-02 13:44:34 +0200

    push created plugin to GIT repo

---
+++ change-author.php
@@ -0,0 +1,58 @@
+<?php
+/**
+Plugin Name: Change Author Alias
+Plugin URI: http://timerecursion.com
+Description: Change Author Alias to a custom alias name when publishing a post. Allows you to specify a custom author name for each post.
+Version: 1.0
+Author: Primož Bevk
+Author URI: http://timerecursion.com
+License: GPL3
+*/
+ 
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/* Admin Author Alias Box */
+add_action('add_meta_boxes', 'ca_add_alias_box');
+function ca_add_alias_box() {
+    $screens = array('post');
+    foreach ($screens as $screen) {
+        add_meta_box(
+            'ca_box_id',
+            'Author Alias',    
+            'ca_alias_meta_box',
+            $screen,
+			'side',
+			'high'
+        );
+    }
+}
+
+function ca_alias_meta_box($post) {
+?>
+   <label for="ca_author_alias">Set the Display name of Post author</label>
+   <input type="text" name="ca_author_alias" id="ca_author_alias" class="postbox" placeholder="John Doe" />
+<?php
+}
+
+/* Save Author Alias data */
+add_action('save_post', 'ca_save_author_meta');
+function ca_save_author_meta($post_id, $post, $update) {
+	if (array_key_exists('ca_author_alias', $_POST) ) {
+        update_post_meta( $post_id,
+           'ca_author_alias',
+            sanitize_text_field($_POST['ca_author_alias']);
+        );
+    }
+}
+
+/* Display Author Alias */
+add_filter('the_author', 'ca_author_filter' );
+function ca_author_filter($name) {
+	global $post;
+	$author = get_post_meta($post->ID, 'ca_author_alias', true);
+	if ($author != '')
+		$name = $author;
+
+	return $name;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Change Author Name ===
+Contributors: baffo
+Tags: author, change, overwrite, post
+Requires at least: 3.5
+Tested up to: 4.3
+Stable tag: 1.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Change Author to a custom alias name when publishing a post.
+
+== Description ==
+
+'Change Author Name' is a plugin, which allows authors to choose custom author aliases when publishing each post, much like Joomla did.
+
+[Plugin site](http://timerecursion.com).
+[GitHub page](https://github.com/baffo/change-author).
+
+== Installation ==
+
+1. Upload `change-author/` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
